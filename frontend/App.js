e27f108a96ee66fafefc68acdf0fd7d0ceb24dec
Ryan Maber <ryan.maber@mention-me.com>
2022-12-21 16:38:14 +0000

    Publish a change with a new component which is uncovered

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,8 @@
 import React, {StrictMode} from "react";
 import {SimpleComponent} from "./components/SimpleComponent";
+import {SimpleUncoveredComponent} from "./components/SimpleUncoveredComponent";
 
 export const App = () => <StrictMode>
     <SimpleComponent />
+    <SimpleUncoveredComponent />
 </StrictMode>;
---
+++ frontend/components/SimpleUncoveredComponent.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const SimpleUncoveredComponent = () => <div>This is a simple frontend component</div>;
