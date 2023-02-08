55b60be92dac83a242cccf3d777edad20baf069c
Ryan Maber <ryan.maber@mention-me.com>
2023-02-08 12:13:15 +0000

    Adding a simple uncovered function

--- src/Service/SimpleService.php
+++ src/Service/SimpleService.php
@@ -15,4 +15,8 @@ class SimpleService
     private function getSecondaryCoveredFunctionMessage(): string {
         return "This is also a covered function (getSecondaryCoveredFunctionMessage). ";
     }
+
+    public function getAnotherUncoveredFunction(): string  {
+        return true;
+    }
 }
